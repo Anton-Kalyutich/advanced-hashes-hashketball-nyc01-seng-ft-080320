d9901ee4ffa30da682a354a51c65a3f2cd6ff0e8
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-22 02:45:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,7 @@ def num_points_scored(name)
   the_hash.each_value do |team_hash|
     team_hash[:players].each do |player|
       if player[:player_name] == name
-        return 
+        return player[:points]
   
 end
 
