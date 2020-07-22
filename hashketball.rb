6c398bfe9826fb402271dbaef00035d0b0f90724
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-22 03:14:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,11 +192,12 @@ end
 
 def big_shoe_rebounds
   size = 0
-  name = ""
+  rebounds = 
   the_hash = game_hash
   the_hash.each_value do |team_hash|
     team_hash[:players].each do |player|
       if player[:shoe] > size
         size = player[:shoe]
+        
       end
 end
